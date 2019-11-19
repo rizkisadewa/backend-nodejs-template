e2a4e87ce708482bce475a1aad2a57d500591d76
Ali Nurdin Sudiana <ansudiana@gmail.com>
2019-11-19 12:58:41 +0700

    added api sms backend for SO Marketing & Nasabah

--- config/sms.js
+++ config/sms.js
@@ -2,3 +2,6 @@ export const baseUrl = 'http://172.31.250.90:8080/MaslahahJSONGateway/JSON';
 export const eventId = 'mdm.dev.10111701';
 export const serviceId = '100016';
 export const channelId = '1000019';
+export const templateSO = 'Demo SMS untuk SO';
+export const templateM = 'Demo SMS untuk Marketing';
+export const templateN = 'Demo SMS untuk Nasabah';
