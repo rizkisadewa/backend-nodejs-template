793163496e5d7ca2859b7df88145234786a2e9c1
rizkisadewa <rizkykysadewa@gmail.com>
2020-01-06 17:01:10 +0700

    push

--- controllers/core.js
+++ controllers/core.js
@@ -197,6 +197,9 @@ class CoreController {
                 }
             });
 
+            console.log(response);
+            console.log(response.data);
+
             resUtil.setSuccess(response.status, response.statusText, response.data);
             return resUtil.send(res+"/"+response);
 
@@ -205,12 +208,13 @@ class CoreController {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 resUtil.setError(error.response.status, error.response.data);
-                return resUtil.send(res);
+                return resUtil.send(res+"/"+response);
             } else if (error.request) {
                 // The request was made but no response was received
                 // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                 // http.ClientRequest in node.js
                 console.log(error.request);
+                console.log(error);
             } else {
                 // Something happened in setting up the request that triggered an Error
                 console.log('Error', error.message);
