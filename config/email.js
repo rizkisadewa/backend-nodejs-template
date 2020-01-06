d150d687aaf26091987555f99b83ce50096659f4
rizkisadewa <rizkykysadewa@gmail.com>
2020-01-06 09:12:39 +0700

    modified email address to tabungan.ibmaslahah@bjbs.co.id

--- config/email.js
+++ config/email.js
@@ -1,8 +1,8 @@
 export const smtp_server = '10.30.246.245'; // 'email.bjbs.co.id';
-export const smtp_auth_username = 'tabungan.ib.maslahah@bjbs.co.id';
+export const smtp_auth_username = 'tabungan.ibmaslahah@bjbs.co.id';
 export const smtp_auth_password = 'Bjbs2019!';
 export const smtp_port = '25';
-export const smtp_sender = 'tabungan.ib.maslahah@bjbs.co.id';
+export const smtp_sender = 'tabungan.ibmaslahah@bjbs.co.id';
 export function emailNasabah(param) {
     const rupiah = new Intl.NumberFormat('id-ID', {
         style: 'currency',
